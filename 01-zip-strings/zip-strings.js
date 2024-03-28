f02544b6fb49a4c51b55e7a53e5845dd112377f3
Bianca Dang <bncdng@gmail.com>
2024-03-28 15:53:33 +0100

    add solution for zipStrings function

--- 01-zip-strings/zip-strings.js
+++ 01-zip-strings/zip-strings.js
@@ -1,3 +1,15 @@
 function zipStrings(strA, strB) {
-  // Your Code here
+  let merged = ""; // Initialize empty string to store merged result
+  let maxLength = Math.max(strA.length, strB.length); // determine length of longest string
+  // iterate through characters with for loop
+  for (let i = 0; i < maxLength; i++) {
+    // put them in merged string
+    if (i < strA.length) {
+      merged += strA[i];
+    }
+    if (i < strB.length) {
+      merged += strB[i];
+    }
+  }
+  return merged; // return merged strings
 }
